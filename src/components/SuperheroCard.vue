7b9f47aba55de293af4f6e34b69339973ea94663
dkersten <dkersten1994@gmail.com>
2021-07-29 19:52:11 -0400

    superhero cards are now loading based on API data

--- src/components/SuperheroCard.vue
+++ src/components/SuperheroCard.vue
@@ -1,15 +1,37 @@
-<template>
+<template v-if="superheroData.length > 0" >
     <div class="superhero-card">
-        <img src="https://cdn.jsdelivr.net/gh/akabab/superhero-api@0.3.0/api/images/sm/1-a-bomb.jpg" />
-        <h3>Hero Name</h3>
-        <span>Full Name</span>
+        <img v-bind:src="heroPic" v-bind:alt="hero.name + ' ' + 'illustrative portrait' " />
+        <h3>Name: {{hero.name}}</h3>
+        <span v-if="hero.biography.fullName !== ''" class="full-name">
+
+            <span class="bold">Full Name:</span> {{hero.biography.fullName}}
+        </span>
+        <!-- <span> {{ stuff }} </span> -->
     </div>
 
 </template>
 
 <script>
+
 export default {
     name: 'SuperheroCard',
+    props: {
+        // mytest: 'testinggg',
+        parentData: Object
+    },
+
+    data() {
+        console.log(this.parentData) 
+        return {
+            hero: this.parentData,
+            heroPic: this.parentData.images.sm
+        }
+    },
+
+    mounted() {
+        console.log(this.parentData)
+    }
+    
 }
 </script>
 
@@ -26,6 +48,7 @@ export default {
     img {
         display: block;
         margin: 0 auto 1.5rem;
+        max-width: 100%;
     }
 
     h3 {
@@ -33,5 +56,13 @@ export default {
         color: #1d1d1f;
         font-size: 1.2rem;
         margin-bottom: 1rem;
+    }
+
+    .full-name {
+        line-height: 1.5;
+    }
+
+    .bold {
+        font-weight: 700;
     }
 </style>
--- src/components/SuperheroContainer.vue
+++ src/components/SuperheroContainer.vue
@@ -1,23 +1,41 @@
 <template>
-    <div class="flex-container">
-        <SuperheroCard />
-        <SuperheroCard />
-        <SuperheroCard />
-        <SuperheroCard />
-        <SuperheroCard />
-        <SuperheroCard />
-        <SuperheroCard />
+    <div>
+        <!-- Create child hero cards only when API data is loaded -->
+        <div class="flex-container" v-if="heros !== null">
+
+            <SuperheroCard class="blah" v-for="hero in heros" :key="hero.id" :parentData="hero" />
+        </div>
+        <!-- Notify user data is loading if connection speed is slow -->
+        <div v-else>
+            <p>Please wait while our heros download...</p>
+        </div>
     </div>
 </template>
 
 <script>
 
+// imports
 import SuperheroCard from './SuperheroCard.vue'
+// import superheroData from '../data.js'
+import axios from 'axios'
 
 export default {
     name: 'SuperHeroContainer',
     components: {
         SuperheroCard
+    },
+
+    data() {
+        return {
+            heros: null,
+            testStuff: [1,2,3,4,5]
+        }
+    },
+
+    mounted() {
+        axios
+            .get('https://akabab.github.io/superhero-api/api/all.json')
+            .then(response => (this.heros = response.data))
     }
 }
 </script>
@@ -27,7 +45,7 @@ export default {
         border: 1px solid #cecece;
         display: flex;
         flex-wrap: wrap;
-        align-items: flex-start;
+        align-items: stretch;
         justify-content: center;
     }
 </style>
